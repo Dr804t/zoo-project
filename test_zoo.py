f529a196e81954cfeb351a1c6fa8afd045d71d63
dr804t <helloworld23531@gmail.com>
2024-11-19 11:54:39 +0700

    finish

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,27 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_invalid_age(self):
+        with self.assertRaises(ValueError):
+            self.zoo.get_ticket_price(-1)
+
     def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(50), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
 
 if __name__ == '__main__':
     unittest.main()
